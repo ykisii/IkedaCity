34c000ca81667c5cc2025af70f160958742657af
ykisii <ishiiyuuki@gmail.com>
2023-05-02 18:33:26 +0900

    add: i/f

--- ikeda_city.ts
+++ ikeda_city.ts
@@ -3,4 +3,12 @@ export class IKedaCity {
   public getPopulation(): number {
     return 0;
   }
+
+  public getCityNames(): any[] {
+    return [];
+  }
+
+  public getPopulationByTownName(name: string): number {
+    return 0;
+  }
 }
--- test.ts
+++ test.ts
@@ -7,3 +7,17 @@ Deno.test (
     assertEquals(103064, new IKedaCity().getPopulation());
   }
 );
+
+Deno.test (
+  "getTownNames",
+  function(): void {
+    assertEquals("", (new IKedaCity().getTownNames())[0]);
+  }
+);
+
+Deno.test (
+  "getPopulationByTownNames",
+  function(): void {
+    assertEquals("", new IKedaCity().getPopulationByTownName(""));
+  }
+);
